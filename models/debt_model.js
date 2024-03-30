61ea93ba23188458bd49ce70b4560e0953fd295b
kursatarslan1 <augenaclash@gmail.com>
2024-03-31 02:06:28 +0300

    debt tracking

--- models/debt_model.js
+++ models/debt_model.js
@@ -1,7 +1,7 @@
-const { client } =  require('../middleware/database');
+const { client } = require('../middleware/database');
 
-class Debt{
-    constructor(debt_id, resident_id, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, status, debit_type){
+class Debt {
+    constructor(debt_id, resident_id, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, status, debit_type) {
         this.debt_id = debt_id;
         this.resident_id = resident_id;
         this.apartment_id = apartment_id;
@@ -16,52 +16,67 @@ class Debt{
         this.debit_type = debit_type;
     }
 
-    static async create(resident_id, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, status, debit_type){
+    static async create(resident_id, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, status, debit_type) {
         const queryText = 'INSERT INTO debt (resident_id, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, status, debit_type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)';
         const values = [resident_id, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, "Not pay", debit_type];
 
-        try{
+        try {
             await client.query(queryText, values);
             return true;
-        } catch (error){
+        } catch (error) {
             console.error('Error creating debt: ', error);
         }
     }
 
-    static async massDebitCreate(apartment_id, description, debit_type, last_payment_date, amount) {
+    static async massDebitCreate(apartment_id, description, debit_type, last_payment_date, amount, include_empty_units) {
         try {
-            const residentQuery = 'SELECT resident_id, block_id, unit_id FROM residents WHERE apartment_id = $1 AND record_status = $2 AND status = $3';
-            const residentResult = await client.query(residentQuery, [apartment_id, 'A', 'A']);
-            
-            for (const row of residentResult.rows) {
-                const resident_id = row.resident_id;
-                const created_at = new Date(); 
-                const payment_date = null; 
-                const block_id = row.block_id;
-                const unit_id = row.unit_id;
-                
-                await this.create(resident_id, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, "Not pay", debit_type);
+            if (include_empty_units) {
+                const queryText = 'SELECT block_id, unit_id FROM units WHERE apartment_id = $1';
+                const queryResult = await client.query(queryText, [apartment_id]);
+
+                for (const row of queryResult.rows) {
+                    const block_id = row.block_id;
+                    const unit_id = row.unit_id;
+                    const created_at = new Date();
+                    const payment_date = null;
+
+                    await this.create(null, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, "Not pay", debit_type);
+                }
+            } else {
+                const queryText = 'SELECT block_id, unit_id FROM units WHERE apartment_id = $1 AND is_using = $2';
+                const values = [apartment_id, 'E'];
+
+                const queryResult = await client.query(queryText, values);
+
+                for (const row of queryResult.rows) {
+                    const block_id = row.block_id;
+                    const unit_id = row.unit_id;
+                    const created_at = new Date();
+                    const payment_date = null;
+
+                    await this.create(null, apartment_id, block_id, unit_id, amount, created_at, payment_date, last_payment_date, description, "Not pay", debit_type);
+                }
             }
-            return true; 
+            return true;
         } catch (error) {
             console.error('Error creating mass debt: ', error);
-            return false; 
+            return false;
         }
     }
 
-    static async getDebtListByResidentId(resident_id){
+    static async getDebtListByResidentId(resident_id) {
         const queryText = 'SELECT * FROM debt WHERE resident_id = $1';
         const values = [resident_id];
 
-        try{
+        try {
             const result = await client.query(queryText, values);
             return result.rows;
-        } catch (error){
+        } catch (error) {
             console.error('Error getting debt list: ', error);
         }
     }
 
-    static async PayDebt(debt_id){
+    static async PayDebt(debt_id) {
         try {
             const query = `
                 UPDATE debt
@@ -73,6 +88,55 @@ class Debt{
             return true;
         } catch (error) {
             console.error('Error updating debt:', error);
+        }
+    }
+
+    static async TotalExpectedRevenue(month, year, apartment_id) {
+        try {
+            const queryText = `SELECT sum(amount) as total_expected_revenue
+            FROM debt
+            WHERE EXTRACT(MONTH FROM created_at) = $1
+              AND EXTRACT(YEAR FROM created_at) = $2
+              AND apartment_id = $3`;
+            const values = [month, year, apartment_id];
+
+            const result = await client.query(queryText, values);
+            return result.rows;
+        } catch (error) {
+            console.log('Error getting total expected revenue: ', error);
+        }
+    }
+
+    static async TotalRevenue(month, year, apartment_id) {
+        try {
+            const queryText = `SELECT sum(amount) as total_expected_revenue
+            FROM debt
+            WHERE EXTRACT(MONTH FROM created_at) = $1
+              AND EXTRACT(YEAR FROM created_at) = $2
+              AND apartment_id = $3
+              AND status = $4`;
+            const values = [month, year, apartment_id, 'Payed'];
+
+            const result = await client.query(queryText, values);
+            return result.rows;
+        } catch (error) {
+            console.log('Error getting total expected revenue: ', error);
+        }
+    }
+
+    static async getDebtUserList(apartment_id) {
+        try {
+            const queryText = `SELECT *
+                FROM residents
+                INNER JOIN debt
+                ON residents.block_id = debt.block_id AND residents.unit_id = debt.unit_id
+                WHERE residents.status = $1 and residents.record_status = $2 and residents.apartment_id = $3`;
+            const values = ['A', 'A', apartment_id];
+
+            const result = await client.query(queryText, values);
+            return result.rows;
+        } catch (error) {
+            console.log('Error getting total expected revenue: ', error);
         }
     }
 }
