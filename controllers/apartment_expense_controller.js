d0dba9aaabbd803ea3f7a517bda68b303a1cb8a2
kursatarslan1 <augenaclash@gmail.com>
2024-04-02 21:19:04 +0300

    one time expenses

--- controllers/apartment_expense_controller.js
+++ controllers/apartment_expense_controller.js
@@ -1,63 +1,169 @@
-const { ApartmentExpense } = require('../models/apartment_expense_model');
+const { ApartmentExpense } = require("../models/apartment_expense_model");
 
-async function createFixedExpense(req, res){
-    const { apartment_id, expense_name, expense_amount, expense_period, expense_content } = req.body;
+async function createFixedExpense(req, res) {
+  const {
+    apartment_id,
+    expense_name,
+    expense_amount,
+    expense_period,
+    expense_content,
+  } = req.body;
 
-    try{
-        const fixedExpenseResult = await ApartmentExpense.createFixedExpense(apartment_id, expense_name, expense_amount, expense_period, expense_content);
+  try {
+    const fixedExpenseResult = await ApartmentExpense.createFixedExpense(
+      apartment_id,
+      expense_name,
+      expense_amount,
+      expense_period,
+      expense_content
+    );
 
-        if(!fixedExpenseResult){
-            res.status(500).json({ success: false });
-        }
-        res.json({ success: true });
-    } catch (error) {
-        res.status(500).json({ success: false });
+    if (!fixedExpenseResult) {
+      res.status(500).json({ success: false });
     }
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
 }
 
-async function getFixedExpenses(req, res){
-    const { apartment_id } = req.query;
+async function createOneTimeExpense(req, res) {
+  const {
+    apartment_id,
+    expense_name,
+    expense_amount,
+    expense_content,
+    expense_date,
+  } = req.body;
 
-    try{
-        const fixedExpensesResult = await ApartmentExpense.getFixedExpenses(apartment_id);
+  try {
+    const oneTimeExpenseResult = await ApartmentExpense.createOneTimeExpense(
+      apartment_id,
+      expense_name,
+      expense_amount,
+      expense_content,
+      expense_date,
+      true
+    );
 
-        if(!fixedExpensesResult){
-            res.status(500).json({ success: false });
-        }
-        res.json({ fixedExpensesResult });
-    } catch (error) {
-        res.status(500).json({ success: false });
+    if (!oneTimeExpenseResult) {
+      res.status(500).json({ success: false });
     }
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
 }
 
-async function deleteFixedExpense(req, res){
-    const {expense_id} = req.query;
+async function getFixedExpenses(req, res) {
+  const { apartment_id } = req.query;
 
-    try{
-        const resultFixedStatus = await ApartmentExpense.deleteFixedExpense(expense_id);
+  try {
+    const fixedExpensesResult = await ApartmentExpense.getFixedExpenses(
+      apartment_id
+    );
 
-        if(!resultFixedStatus){
-            res.status(500).json({ success: false});
-        }
-
-        res.json({success: true});
-    }catch(error){
-        res.status(500).json({success: false});
+    if (!fixedExpensesResult) {
+      res.status(500).json({ success: false });
     }
+    res.json({ fixedExpensesResult });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
 }
 
-async function updateFixedExpense(req, res){
-    const { expense_id, expense_name, expense_amount, expense_period, expense_content } = req.body;
+async function getOneTimeExpenses(req, res) {
+  const { apartment_id } = req.query;
 
-    try{
-        const updateFixedExpenseResult = await ApartmentExpense.updateFixedExpense(expense_id, expense_name, expense_amount, expense_period, expense_content);
-        if(!updateFixedExpense){
-            res.json({ success: false });
-        }
-        res.json({ success: true });
-    } catch (error){
-        res.json({ success: false });
-    }
+  try {
+    const oneTimeexpenseResult = await ApartmentExpense.getOneTimeExpenses(
+      apartment_id
+    );
+
+    if (!oneTimeexpenseResult) res.status(500).json({ success: false });
+
+    res.json({ oneTimeexpenseResult });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
 }
 
-module.exports = {createFixedExpense, getFixedExpenses , deleteFixedExpense, updateFixedExpense }
+async function deleteFixedExpense(req, res) {
+  const { expense_id } = req.query;
+
+  try {
+    const resultFixedStatus = await ApartmentExpense.deleteFixedExpense(
+      expense_id
+    );
+
+    if (!resultFixedStatus) {
+      res.status(500).json({ success: false });
+    }
+
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
+}
+
+async function updateOneTimeexpense(req, res) {
+  const {
+    expense_id,
+    expense_name,
+    expense_amount,
+    expense_date,
+    expense_content,
+  } = req.body;
+
+  try {
+    const updateOneTimeExpenseResult =
+      await ApartmentExpense.updateOneTimeExpense(
+        expense_id,
+        expense_name,
+        expense_amount,
+        expense_date,
+        expense_content
+      );
+    if (!updateOneTimeExpenseResult) res.status(500).json({ success: false });
+
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
+}
+
+async function updateFixedExpense(req, res) {
+  const {
+    expense_id,
+    expense_name,
+    expense_amount,
+    expense_period,
+    expense_content,
+  } = req.body;
+
+  try {
+    const updateFixedExpenseResult = await ApartmentExpense.updateFixedExpense(
+      expense_id,
+      expense_name,
+      expense_amount,
+      expense_period,
+      expense_content
+    );
+    if (!updateFixedExpense) {
+      res.json({ success: false });
+    }
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false });
+  }
+}
+
+module.exports = {
+  createFixedExpense,
+  createOneTimeExpense,
+  getFixedExpenses,
+  getOneTimeExpenses,
+  deleteFixedExpense,
+  updateOneTimeexpense,
+  updateFixedExpense,
+};
--- models/apartment_expense_model.js
+++ models/apartment_expense_model.js
@@ -1,68 +1,166 @@
-const { client } = require('../middleware/database');
+const { client } = require("../middleware/database");
 
-class ApartmentExpense{
-    constructor(expense_id, apartment_id, expense_name, expense_amount, expense_date, expense_period, is_one_time_expense){
-        this.expense_id = expense_id;
-        this.apartment_id = apartment_id;
-        this.expense_name = expense_name;
-        this.expense_amount = expense_amount;
-        this.expense_date = expense_date;
-        this.expense_period = expense_period;
-        this.is_one_time_expense = is_one_time_expense;
+class ApartmentExpense {
+  constructor(
+    expense_id,
+    apartment_id,
+    expense_name,
+    expense_amount,
+    expense_date,
+    expense_period,
+    is_one_time_expense
+  ) {
+    this.expense_id = expense_id;
+    this.apartment_id = apartment_id;
+    this.expense_name = expense_name;
+    this.expense_amount = expense_amount;
+    this.expense_date = expense_date;
+    this.expense_period = expense_period;
+    this.is_one_time_expense = is_one_time_expense;
+  }
+
+  static async createFixedExpense(
+    apartment_id,
+    expense_name,
+    expense_amount,
+    expense_period,
+    expense_content
+  ) {
+    const queryText =
+      "INSERT INTO apartment_expense (apartment_id, expense_name, expense_amount, expense_date, expense_period, is_one_time_expense, expense_content) VALUES($1,$2,$3,$4,$5,$6, $7)";
+    const values = [
+      apartment_id,
+      expense_name,
+      expense_amount,
+      new Date(),
+      expense_period,
+      false,
+      expense_content,
+    ];
+
+    try {
+      await client.query(queryText, values);
+      return true;
+    } catch (error) {
+      console.log("Error creating fixed expense: ", error);
     }
+  }
 
-    static async createFixedExpense(apartment_id, expense_name, expense_amount, expense_period, expense_content){
-        const queryText = 'INSERT INTO apartment_expense (apartment_id, expense_name, expense_amount, expense_date, expense_period, is_one_time_expense, expense_content) VALUES($1,$2,$3,$4,$5,$6, $7)';
-        const values = [apartment_id, expense_name, expense_amount, new Date(), expense_period, false, expense_content];
+  static async createOneTimeExpense(
+    apartment_id,
+    expense_name,
+    expense_amount,
+    expense_content,
+    expense_date,
+    is_one_time_expense
+  ) {
+    const queryText =
+      "INSERT INTO apartment_expense (apartment_id, expense_name, expense_amount, expense_content, expense_date, is_one_time_expense) VALUES($1, $2, $3, $4, $5, $6);";
+    const values = [
+      apartment_id,
+      expense_name,
+      expense_amount,
+      expense_content,
+      expense_date,
+      is_one_time_expense,
+    ];
 
-        try{
-            await client.query(queryText, values);
-            return true;
-        } catch (error) {
-            console.log('Error creating fixed expense: ', error);
-        }
+    try {
+      await client.query(queryText, values);
+      return true;
+    } catch (error) {
+      console.log("Error creating one time expense", error);
     }
+  }
 
-    static async getFixedExpenses(apartment_id){
-        const queryText = 'SELECT * FROM apartment_expense WHERE apartment_id = $1';
-        const values = [apartment_id];
+  static async getFixedExpenses(apartment_id) {
+    const queryText =
+      "SELECT * FROM apartment_expense WHERE apartment_id = $1 AND is_one_time_expense = $2;";
+    const values = [apartment_id, false];
 
-        try{
-            const result = await client.query(queryText, values);
-            return result.rows;
-        } catch (error) {
-            console.log('Error getting fixed expense: ', error);
-        }
+    try {
+      const result = await client.query(queryText, values);
+      return result.rows;
+    } catch (error) {
+      console.log("Error getting fixed expense: ", error);
     }
+  }
 
-    static async deleteFixedExpense(expense_id){
-        const queryText = 'DELETE FROM apartment_expense WHERE expense_id = $1';
-        const values = [expense_id];
+  static async getOneTimeExpenses(apartment_id) {
+    const queryText =
+      "SELECT * FROM apartment_expense WHERE apartment_id = $1 AND is_one_time_expense = $2;";
+    const values = [apartment_id, true];
 
-        try{
-            await client.query(queryText, values);
-            return true; 
-        }catch(error){
-            console.log(error);
-        }
-
+    try {
+      const result = await client.query(queryText, values);
+      return result.rows;
+    } catch (error) {
+      console.log("Error getting one time expenses: ", error);
     }
+  }
 
-    static async updateFixedExpense(expense_id, expense_name, expense_amount, expense_period, expense_content){
-        const query = {
-            text: 'UPDATE apartment_expense SET expense_name = $2, expense_amount = $3, expense_period = $4, expense_content = $5 WHERE expense_id = $1',
-            values: [expense_id, expense_name, expense_amount, expense_period, expense_content]
-        };
-    
-        try {
-            await client.query(query);
-            console.log("Expense updated successfully");
-            return true;
-        } catch (err) {
-            console.error("Error updating expense:", err);
-            throw err;
-        }
+  static async deleteFixedExpense(expense_id) {
+    const queryText = "DELETE FROM apartment_expense WHERE expense_id = $1";
+    const values = [expense_id];
+
+    try {
+      await client.query(queryText, values);
+      return true;
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  static async updateOneTimeExpense(
+    expense_id,
+    expense_name,
+    expense_amount,
+    expense_date,
+    expense_content
+  ) {
+    const query = {
+      text: "UPDATE apartment_expense SET expense_name = $2, expense_amount = $3, expense_date = $4, expense_content = $5 WHERE expense_id = $1",
+      values: [
+        expense_id,
+        expense_name,
+        expense_amount,
+        expense_date,
+        expense_content,
+      ],
+    };
+    try {
+      await client.query(query);
+      return true;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  static async updateFixedExpense(
+    expense_id,
+    expense_name,
+    expense_amount,
+    expense_period,
+    expense_content
+  ) {
+    const query = {
+      text: "UPDATE apartment_expense SET expense_name = $2, expense_amount = $3, expense_period = $4, expense_content = $5 WHERE expense_id = $1",
+      values: [
+        expense_id,
+        expense_name,
+        expense_amount,
+        expense_period,
+        expense_content,
+      ],
+    };
+
+    try {
+      await client.query(query);
+      return true;
+    } catch (err) {
+      throw err;
+    }
+  }
 }
 
 module.exports = { ApartmentExpense };
